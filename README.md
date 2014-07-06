e5653ff4ee11efc32fa8c473c3c513d018abd141
Colin O'Dell <colinodell@gmail.com>
2014-07-06 17:56:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+lync-productivity-timer
+=======================
+
+Productivity timer which allows you to work without interuption by automatically setting your Microsoft Lync availability to "Do Not Disturb" for a short while.
+
+Works great with [Pomodoro](http://pomodorotechnique.com/) and other time management techniques.
+
+## Requirements:
+
+I'm using the following to develop the app:
+
+* Microsoft Lync 2010
+* [Microsoft Lync 2010 SDK](http://www.microsoft.com/en-us/download/details.aspx?id=18898)
+* Visual Studio 2010
+* Windows 7
+
+I'm unsure if this works with other versions - YMMV.
+
+## Contributing
+
+Any/all contributions would be greatly appreciated!  This is a small, personal project, so please be patient if I don't immediately respond to any issues or pull requests.
+
+## Legal Stuff
+
+Copyright (c) 2014 Colin O'Dell. Released under [the MIT License](LICENSE.md).
+
+This app is neither associated with nor endorsed by [The Pomodoro Technique](http://pomodorotechnique.com/) or it's owners/creators.
---
+++ src/lync-productivity-timer/Timer.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Lync.Model;
+
+namespace lync_productivity_timer
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Timer : Window
+    {
+        private DispatcherTimer updateTimer;
+        private short minutes;
+
+        public Timer()
+        {
+            InitializeComponent();
+            updateTimer = new DispatcherTimer();
+            updateTimer.Interval = new TimeSpan(0, 1, 0);
+            updateTimer.Tick += new EventHandler(updateTimer_Tick);
+
+            txtAwayMessageTemplate.Text = "In 'flow' for the next {0}. Please wait until {1} or email me if it's not urgent. Thanks!";
+            lblReplacements.Content = "Replacements:" + Environment.NewLine + "  {0}: 'xx minute(s)' - countdown" + Environment.NewLine + "  {1}: 'xx:xx' - end time";
+        }
+
+        void updateTimer_Tick(object sender, EventArgs e)
+        {
+            minutes -= 1;
+            if (minutes <= 0)
+            {
+                // Alarm!
+                btnStop_Click(this, null);
+            }
+            else
+            {
+                string remainaing = String.Format("{0} minute{1}", minutes, minutes == 1 ? "" : "s");
+                string endTime = DateTime.Now.AddMinutes(minutes).ToShortTimeString();
+
+                // Update countdown label                
+                lblTimeRemaining.Content = String.Format("{0} ({1})", remainaing, endTime);
+                // Update status
+                UpdateLync(PublishableContactInformationType.PersonalNote, String.Format(txtAwayMessageTemplate.Text, remainaing, endTime));
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            
+            minutes = Int16.Parse(txtLength.Text);
+            updateTimer.Start();
+            
+            UpdateLync(PublishableContactInformationType.Availability, ContactAvailability.DoNotDisturb);    
+
+            minutes++;
+            updateTimer_Tick(this, null);
+
+            btnStop.IsEnabled = true;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            btnStop.IsEnabled = false;
+
+            updateTimer.Stop();
+
+            UpdateLync(PublishableContactInformationType.Availability, ContactAvailability.Free);
+            UpdateLync(PublishableContactInformationType.PersonalNote, "");
+
+            lblTimeRemaining.Content = "0 minutes";
+           
+            btnStart.IsEnabled = true;
+        }
+
+        private void UpdateLync(PublishableContactInformationType type, object value)
+        {
+            var client = LyncClient.GetClient();
+            var contactInfo = new Dictionary<PublishableContactInformationType, object>();
+            contactInfo.Add(type, value);
+            try
+            {
+
+                var publish = client.Self.BeginPublishContactInformation(contactInfo, null, null);
+                client.Self.EndPublishContactInformation(publish);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("(c) 2014 Colin O'Dell", "About " + this.Title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
